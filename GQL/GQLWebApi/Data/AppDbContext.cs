df428fb1359ef9d21c00670fcf5be8ae592b7fa3
Muhammad Razali <wireless-@hotmail.com>
2021-12-18 08:49:02 +0800

    init appdbcontext

---
+++ GQL/GQLWebApi/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GQLWebApi.Data.Configurations;
+using GQLWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GQLWebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOptions) : base (dbContextOptions)    
+        {
+
+        }
+
+        public DbSet<Platform>  Platforms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PlatformConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
