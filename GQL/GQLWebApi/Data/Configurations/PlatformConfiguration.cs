710ff637df80d2b2b8442a03f45670758d95bdd6
Muhammad Razali <wireless-@hotmail.com>
2021-12-18 08:48:53 +0800

    Added Platform Config

---
+++ GQL/GQLWebApi/Data/Configurations/PlatformConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using GQLWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GQLWebApi.Data.Configurations
+{
+    public class PlatformConfiguration : IEntityTypeConfiguration<Platform>
+    {
+        public void Configure(EntityTypeBuilder<Platform> builder)
+        {
+            builder
+                .ToTable("PlatForms");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.LicenseKey)
+                .HasMaxLength(100);
+
+        }
+    }
+}
