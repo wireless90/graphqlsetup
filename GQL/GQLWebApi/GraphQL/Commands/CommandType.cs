991a2772649325b05438753cc908a5ebdb14d391
Muhammad Razali <wireless-@hotmail.com>
2021-12-19 16:46:54 +0800

    Add a new field which is not in model at all

--- GQL/GQLWebApi/GraphQL/Commands/CommandType.cs
+++ GQL/GQLWebApi/GraphQL/Commands/CommandType.cs
@@ -9,8 +9,24 @@ namespace GQLWebApi.GraphQL.Commands
             /* How to change field name */
             descriptor
                 .Field(x => x.CommandLine)
-                .Name("CmdLine");
+                .Name("cmdLine");
 
+            /* Add a new field which is not in model at all */
+            descriptor
+                .Field("haveArgument")
+                .Type<BooleanType>()
+                .Resolve(context =>
+                {
+                    Command command = context.Parent<Command>();
+
+                    if (command.CommandLine == null)
+                        return false;
+
+                    return command.CommandLine?
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Count() > 1;
+
+                });
             base.Configure(descriptor);
         }
     }
